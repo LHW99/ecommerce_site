f44678553390fa70fbb75f573b987e9e5b0d2666
haiduk <hwlee91@gmail.com>
2021-04-25 02:54:45 -0400

    category funcationality

--- ecommerce_vue/src/views/Home.vue
+++ ecommerce_vue/src/views/Home.vue
@@ -16,28 +16,19 @@
         <h2 class='is-size-2 has-text-centered'>Latest Products</h2>
       </div>
 
-      <div 
-        class='column is-3' 
-        v-for="product in latestProducts"
-        v-bind:key="product.id"
-      >
-        <div class='box'>
-          <figure class='image mb-4'>
-            <img :src="product.get_thumbnail">
-          </figure>
-
-          <h3 class='is-size-4'>{{ product.name }}</h3>
-          <p class='is-size-6 has-text-grey'>${{ product.price }}</p>
-
-          <router-link v-bind:to='product.get_absolute_url' class='button is-dark mt-4'>View Details</router-link>
-        </div>
-      </div>
+      <ProductBox
+        v-for='product in latestProducts'
+        v-bind:key='product.id'
+        v-bind:product='product' />
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+
+import ProductBox from '@/components/ProductBox'
+
 export default {
   name: 'Home',
   data() {
@@ -46,9 +37,12 @@ export default {
     }
   },
   components: {
+    ProductBox
   },
   mounted() {
     this.getLatestProducts()
+
+    document.title = 'Home | Djackets'
   },
   methods: {
     async getLatestProducts() {
@@ -69,10 +63,3 @@ export default {
 }
 </script>
 
-<style scoped>
-  .image {
-    margin-top: -1.25rem;
-    margin-left: -1.25rem;
-    margin-right: -1.25rem;
-  }
-</style>
