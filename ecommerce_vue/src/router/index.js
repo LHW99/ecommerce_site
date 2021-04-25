e11eef0d4e7a20a2efa396f31ca9962866b00dcf
haiduk <hwlee91@gmail.com>
2021-04-25 03:17:35 -0400

    search functionality

--- ecommerce_vue/src/router/index.js
+++ ecommerce_vue/src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
+import Search from '../views/Search.vue'
 
 const routes = [
   {
@@ -19,6 +20,11 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path: '/search',
+    name: 'Search',
+    component: Search
+  },
+  {
     path: '/:category_slug/:product_slug/',
     name: 'Product',
     component: Product,
@@ -27,7 +33,7 @@ const routes = [
     path: '/:category_slug',
     name: 'Category',
     component: Category,
-  }
+  },
 ]
 
 const router = createRouter({
